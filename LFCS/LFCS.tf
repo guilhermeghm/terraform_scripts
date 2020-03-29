498cf2959697f90c5712c1ec4acc7f6021841ff8
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-03-29 18:12:10 +0200

    Adding LFCS script

---
+++ LFCS/LFCS.tf
@@ -0,0 +1,58 @@
+##################################################################
+# Base configuration
+##################################################################
+
+provider "aws" {
+  region                  = "eu-west-1"
+  shared_credentials_file = "/Users/macielgm/.aws/credentials"
+  profile                 = "default"
+}
+
+##################################################################
+# EC2 creation
+##################################################################
+
+resource "aws_instance" "EC2_server1" {
+  ami           = "ami-096782d2a9225fe7a"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-7552d20f"]
+  subnet_id = "subnet-d8148ebf"
+  key_name = "awssupport"
+  tags = {
+    Name = "LFCS_server1"
+    auto-delete = "no"
+    auto-stop = "no"
+  }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            yum update -y
+            yum install gcc telnet nc vim -y
+            hostnamectl set-hostname server1.mydomain.local
+            echo "ClientAliveInterval 120" >> /etc/ssh/sshd_config
+            systemctl restart sshd
+            EOF
+}
+
+
+resource "aws_instance" "EC2_server2" {
+  ami           = "ami-096782d2a9225fe7a"
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-7552d20f"]
+  subnet_id = "subnet-d8148ebf"
+  key_name = "awssupport"
+  tags = {
+    Name = "LFCS_server2"
+    auto-delete = "no"
+    auto-stop = "no"
+  }
+
+  user_data = <<-EOF
+            #!/bin/bash
+            yum update -y
+            yum install gcc telnet nc vim -y
+            hostnamectl set-hostname server1.mydomain.local
+            echo "ClientAliveInterval 120" >> /etc/ssh/sshd_config
+            systemctl restart sshd
+            EOF
+}
