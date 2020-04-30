3dee181bad179bde03bcb287e8a86ce125ea43b7
Guilherme Maciel <ghm.guilherme@gmail.com>
2020-04-30 17:47:31 +0200

    Changing Terraform version to 0.12

--- LFCS/LFCS.tf
+++ LFCS/LFCS.tf
@@ -13,15 +13,15 @@ provider "aws" {
 ##################################################################
 
 resource "aws_instance" "EC2_server1" {
-  ami           = "ami-0a3c2e3ecfcddf2d1"
-  instance_type = "t3.medium"
+  ami                    = "ami-0a3c2e3ecfcddf2d1"
+  instance_type          = "t3.medium"
   vpc_security_group_ids = ["sg-7552d20f"]
-  subnet_id = "subnet-d8148ebf"
-  key_name = "awssupport"
+  subnet_id              = "subnet-d8148ebf"
+  key_name               = "awssupport"
   tags = {
-    Name = "LFCS_server1"
+    Name        = "LFCS_server1"
     auto-delete = "no"
-    auto-stop = "no"
+    auto-stop   = "no"
   }
 
   user_data = <<-EOF
@@ -31,20 +31,20 @@ resource "aws_instance" "EC2_server1" {
             hostnamectl set-hostname server1.mydomain.local
             echo "ClientAliveInterval 120" >> /etc/ssh/sshd_config
             systemctl restart sshd
-            EOF
+EOF
+
 }
 
-
 resource "aws_instance" "EC2_server2" {
-  ami           = "ami-0a3c2e3ecfcddf2d1"
-  instance_type = "t3.medium"
+  ami                    = "ami-0a3c2e3ecfcddf2d1"
+  instance_type          = "t3.medium"
   vpc_security_group_ids = ["sg-7552d20f"]
-  subnet_id = "subnet-d8148ebf"
-  key_name = "awssupport"
+  subnet_id              = "subnet-d8148ebf"
+  key_name               = "awssupport"
   tags = {
-    Name = "LFCS_server2"
+    Name        = "LFCS_server2"
     auto-delete = "no"
-    auto-stop = "no"
+    auto-stop   = "no"
   }
 
   user_data = <<-EOF
@@ -54,5 +54,7 @@ resource "aws_instance" "EC2_server2" {
             hostnamectl set-hostname server2.mydomain.local
             echo "ClientAliveInterval 120" >> /etc/ssh/sshd_config
             systemctl restart sshd
-            EOF
+EOF
+
 }
+
